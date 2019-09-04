9048312d16ae88fc10623b78593da75ffe705fbf
Gene Laisne <glaisne@gmail.com>
2019-09-04 17:20:30 -0400

    Initial version

---
+++ Azure Update Management/AzureUpdateManagement.ps1
@@ -0,0 +1,21 @@
+<################################################################
+
+Setup requirements
+
+1) VM
+    a) VM extension 
+2) Log Analytics
+
+# resources:
+ - https://docs.microsoft.com/en-us/azure/automation/automation-update-management
+##################################################################>
+
+# Add a system via the Update Management blade
+# Add a system via the Update Management blade in Automation.
+# Add an on-prem system.
+## https://www.tech-coffee.net/getting-started-with-azure-update-management-to-handle-windows-updates/
+## https://docs.microsoft.com/en-us/azure/azure-monitor/platform/agent-windows
+### installing the agent, confirming connection, configurint tls 1.2...
+
+# Configure TLS 1.2 on VM
+# https://docs.microsoft.com/en-us/windows-server/security/tls/tls-registry-settings#tls-12
